6486d3b4bbba46c32915bcb96012a5f8542d1e43
Marcel <marcelc6363@gmail.com>
2021-10-23 16:18:03 +0700

    Update API route

--- api/auth.ts
+++ api/auth.ts
@@ -1,9 +1,13 @@
 import API from '~/api/client'
 
 export async function login(payload: object) {
-  return await API.handle(API.public.post(`/api/v1/login`, payload))
+  return await API.handle(API.public.post(`/api/public/login`, payload))
 }
 
 export async function logout() {
   return await API.handle(API.public.post(`/api/v1/logout`))
+}
+
+export async function check() {
+  return await API.handle(API.public.post(`/api/v1/check`))
 }
--- api/images.ts
+++ api/images.ts
@@ -1,10 +1,5 @@
 import API from '~/api/client'
 
 export async function uploadImage(file: any) {
-  return await API.handle(
-    API.privateFormData.post(
-      `/api/v1/auth/upload_image/patient_identifier`,
-      file
-    )
-  )
+  return await API.handle(API.privateFormData.post(`/api/v1/images`, file))
 }
